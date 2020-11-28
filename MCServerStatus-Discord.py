49f7f76288b34fc5488b1297d40d9bd801858d8b
Jahus <jahus@live.ie>
2020-11-28 19:28:06 +0100

    0.4 - Use async wait and add log messages.

--- MCServerStatus-Discord.py
+++ MCServerStatus-Discord.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 
-import discord, json, requests, time, emoji, datetime
+import discord, json, requests, time, emoji
+from datetime import datetime, timezone, timedelta
+from asyncio import sleep
 
 
 with open("config.json", "r") as _f:
@@ -8,7 +10,7 @@ with open("config.json", "r") as _f:
 
 
 __token= config["discord_bot_token"]
-__sleep_time = 300
+__sleep_time = 30
 __client = discord.Client()
 __colors = {
     "online": 0x00ff00,
@@ -18,7 +20,7 @@ __colors = {
 
 @__client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(__client))
+    print('Bot logged in as {0.user}.'.format(__client))
     await mc_server_status()
 
 
@@ -26,14 +28,17 @@ async def mc_server_status():
     try:
         with open("last_message.id", "r") as _f:
             _embed_message_id = int(_f.read())
+            print("Found stored message id %i" % _embed_message_id)
     except:
         _embed_message_id = -1
+        print("Nothing stored. Id set to -1.")
     while True:
         _req = requests.get("https://api.mcsrvstat.us/2/%s" % config["server_address"])
         if _req.status_code != 200:
             print("Error contacting API : %s" % _req.status_code)
         else:
             _req_json = _req.json()
+            print("Contacted API correctly.")
             if not _req_json["online"]:
                 _embed = discord.Embed(
                     title="Kabyle Minecraft",
@@ -44,6 +49,7 @@ async def mc_server_status():
                     value="%s Hors ligne" % emoji.emojize(":red_circle:"),
                     inline=False
                 )
+                print("Server offline. Created embed.")
             else:
                 _embed = discord.Embed(
                     title="Kabyle Minecraft",
@@ -64,31 +70,46 @@ async def mc_server_status():
                         value=_server_data[_key],
                         inline=False
                     )
+                print("Server online. Created embed.")
             _embed.add_field(
                 name="Dernière mise à jour",
-                value="%s `%s`" % (emoji.emojize(":arrows_counterclockwise:"),datetime.datetime.strftime(datetime.datetime.now(tz=datetime.timezone(datetime.timedelta(hours=1))), "%H:%M:%S"))
+                value="%s `%s`" % (emoji.emojize(":arrows_counterclockwise:"),datetime.strftime(datetime.now(tz=timezone(timedelta(hours=1))), "%H:%M:%S"))
             )
+            print("Added timestamp to embed.")
             if _embed_message_id == -1:
                 try:
+                    print("Creating and sending message.")
                     _channel = __client.get_channel(config["discord_channel_id"])
                     _embed_message = await _channel.send(embed=_embed)
                     _embed_message_id = _embed_message.id
+                    print("Message created correctly.")
                 except:
                     print("Error: Could not send message to channel.")
             else:
                 try:
+                    print("Getting old message with id %i" % _embed_message_id)
                     _channel = __client.get_channel(config["discord_channel_id"])
                     _embed_message = await _channel.fetch_message(_embed_message_id)
+                    print("Editing the old message...")
                     await _embed_message.edit(embed=_embed)
+                    print("Edited the old message correctly.")
                 except discord.errors.NotFound:
+                    print("Could not find message to edit.")
                     _embed_message_id = -1
                 except discord.errors.Forbidden:
                     # You do not have the permissions required to get a message.
                     print("Error: Please, allow the bot to view ancient messages.")
                     _embed_message_id = -1
-            with open("last_message.id", "w") as _f:
-                _f.write(str(_embed_message_id))
-        time.sleep(__sleep_time)
+            try:
+                with open("last_message.id", "w") as _f:
+                    _f.write(str(_embed_message_id))
+                print("Message data saved correctly.")
+            except:
+                print("Could not save data.")
+                pass
+        print("Sleeping for %i s" % __sleep_time)
+        await sleep(__sleep_time)
+        print("Wait complete.")
 
 
 __client.run(__token)
